9e9b6abf1819a11b7da5bc823376937f0b3f1d43
Kinetic639 <michal.stepien36@gmail.com>
2022-06-14 12:31:55 +0200

    init commit

---
+++ server/index.ts
@@ -0,0 +1,17 @@
+import express, {json} from "express";
+import cors from "cors";
+import 'express-async-errors'
+
+const app = express()
+
+app.use(cors({
+    origin: 'http://localhost:3000'
+}))
+
+app.use(json())
+
+const port = 3001
+app.listen(port, '0.0.0.0', () => {
+    console.log(`listening on http://localhost:${port}`)
+})
+
