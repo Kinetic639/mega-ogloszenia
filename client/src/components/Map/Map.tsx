c226d40de4b1e43e510eb8f706fdc75f41e4e711
Kinetic639 <michal.stepien36@gmail.com>
2022-06-16 16:12:48 +0200

    Added map support and fixed missing marker icons

--- client/src/components/Map/Map.tsx
+++ client/src/components/Map/Map.tsx
@@ -1,9 +1,26 @@
 import React from "react";
+import {MapContainer, Marker, Popup, TileLayer} from "react-leaflet";
+import Box from "@mui/material/Box";
+import '../../utils/fix-map-icons'
+
+import 'leaflet/dist/leaflet.css';
+import './Map.scss'
 
 export const Map = () => {
     return (
-        <>
-            Map
-        </>
+        <Box sx={{height: {xs: 'calc(100vh - 56px)', sm: 'calc(100vh - 64px)'}}} className='map'>
+            <MapContainer center={[52.4347477, 16.9233696]} zoom={16}>
+                <TileLayer
+                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                    attribution="&copy; <a href='https://www.openstreetmap.org/copyright'>OpenStreetMap</a> & contributors"
+                />
+                <Marker position={[52.43455, 16.92175]}>
+                    <Popup>
+                        <h2>Moja chata</h2>
+                        <p>Widziana z mojej apki</p>
+                    </Popup>
+                </Marker>
+            </MapContainer>
+        </Box>
     )
 }
