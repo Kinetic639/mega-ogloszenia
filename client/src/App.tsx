ad8738f41dd966dea267d3899f1e086a033dbd2d
Kinetic639 <michal.stepien36@gmail.com>
2022-06-16 16:10:39 +0200

    added custom mui theme colors

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,40 @@
 import React from 'react';
 import {Layout} from "./components/Layout/Layout";
+import {createTheme, ThemeProvider} from '@mui/material/styles';
+import {orange, teal} from '@mui/material/colors';
+
+declare module '@mui/material/styles' {
+    interface Theme {
+        status: {
+            danger: string;
+        };
+    }
+
+    // allow configuration using `createTheme`
+    interface ThemeOptions {
+        status?: {
+            danger?: string;
+        };
+    }
+}
+
+const theme = createTheme({
+    status: {
+        danger: orange[500],
+    },
+    palette: {
+        primary: teal,
+        secondary: {
+            main: '#b2dfdb',
+        },
+    },
+});
 
 export const App = () => {
     return (
-        <Layout/>
+        <ThemeProvider theme={theme}>
+            <Layout/>
+        </ThemeProvider>
     )
 }
 
