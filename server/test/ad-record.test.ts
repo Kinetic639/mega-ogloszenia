7b74b366423a5af5179c38a3e52a387bba3529f2
Kinetic639 <michal.stepien36@gmail.com>
2022-06-25 19:08:09 +0200

    Added jest and first tests to project

---
+++ server/test/ad-record.test.ts
@@ -0,0 +1,26 @@
+import {AdRecord} from "../records/ad.record";
+
+const defaultObj = {
+    name: 'Test Name',
+    description: 'Test Description',
+    url: ' https://test.pl',
+    lat: 9,
+    lon: 9,
+    price: 123
+}
+
+test('Can build AdRecord', () => {
+    const ad = new AdRecord(defaultObj)
+
+    expect(ad.name).toBe('Test Name')
+    expect(ad.description).toBe('Test Description')
+})
+
+test('Can build AdRecord', () => {
+    expect(() => new AdRecord({
+        ...defaultObj, price: -4
+    })).toThrow('Cena produktu musi być większa od 0 i mniejsza od 9999999.')
+})
+
+
+//@TODO: Check rest of validations
