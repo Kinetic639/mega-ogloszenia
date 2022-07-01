af45b7dd8b84b805236173a19ebdf96b8324ad4c
Kinetic639 <michal.stepien36@gmail.com>
2022-07-01 20:25:45 +0200

    Map and Markers are finished!

--- client/src/components/Map/Map.tsx
+++ client/src/components/Map/Map.tsx
@@ -1,4 +1,4 @@
-import React, {useContext, useEffect} from "react";
+import React, {useContext, useEffect, useState} from "react";
 import {MapContainer, Marker, Popup, TileLayer} from "react-leaflet";
 import Box from "@mui/material/Box";
 import '../../utils/fix-map-icons'
@@ -6,28 +6,36 @@ import '../../utils/fix-map-icons'
 import 'leaflet/dist/leaflet.css';
 import './Map.scss'
 import {SearchContext} from "../../context/search-context";
+import {SimpleAdEntity} from "types";
+import {SingleAd} from "./SingleAd";
 
 export const Map = () => {
     const {search} = useContext(SearchContext)
+    const [ads, setAds] = useState<SimpleAdEntity[]>([])
 
     useEffect(() => {
-        
+        (async () => {
+            const res = await fetch(`http://localhost:3001/ad/search/${search}`)
+            const data = await res.json()
+
+            setAds(data)
+        })()
     }, [search])
 
     return (
         <Box sx={{height: {xs: 'calc(100vh - 56px)', sm: 'calc(100vh - 64px)'}}} className='map'>
-            <h1>text: {search}</h1>
             <MapContainer center={[52.4347477, 16.9233696]} zoom={16}>
                 <TileLayer
                     url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                     attribution="&copy; <a href='https://www.openstreetmap.org/copyright'>OpenStreetMap</a> & contributors"
                 />
-                <Marker position={[52.43455, 16.92175]}>
-                    <Popup>
-                        <h2>Moja chata</h2>
-                        <p>Widziana z mojej apki</p>
-                    </Popup>
-                </Marker>
+                {ads.map(ad => (
+                    <Marker key={ad.id} position={[ad.lat, ad.lon]}>
+                        <Popup>
+                            <SingleAd id={ad.id}/>
+                        </Popup>
+                    </Marker>
+                ))}
             </MapContainer>
         </Box>
     )
